82445bc986f26299b705982d2a6909baf6f78181
SkyO-ne <SkyOne@users.noreply.github.com>
2019-08-02 21:05:18 -0700

    working mostly

--- myRobot.js
+++ myRobot.js
@@ -1,10 +1,12 @@
+var canvas, gl, program;
 
-var canvas;
-var gl;
-var vertexCount = 36;
-var program;
+var points = [];
+var colors = [];
+var displancement = 1;
 
-// Three Vertices        
+var ballMouseLocation = [];
+var thetaL, thetaU;
+
 var vertices = [
     vec4( -0.5, -0.5,  0.5, 1.0 ),
     vec4( -0.5,  0.5,  0.5, 1.0 ),
@@ -15,64 +17,127 @@ var vertices = [
     vec4(  0.5,  0.5, -0.5, 1.0 ),
     vec4(  0.5, -0.5, -0.5, 1.0 )
 ];
-var vertexColor = [
-    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-    vec4( 0.1, 0.7, 0, 1.0 ),  // white
-    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+var vertexCount = 36;
+
+var colorTmp = {
+  black: vec4( 0.0, 0.0, 0.0, 0.7 ),
+  white: vec4( 0.1, 0.7, 0, 0.7 ),
+  blue: vec4( 0.0, 0.0, 1.0, 0.7 ),
+  yellow: vec4( 1.0, 1.0, 0.0, 0.7 ),
+  green: vec4( 0.0, 1.0, 0.0, 0.7 ),
+  red: vec4( 1.0, 0.0, 0.0, 0.7 ),
+  magenta: vec4( 1.0, 0.0, 1.0, 0.7 ),
+  cyan: vec4( 0.0, 1.0, 1.0, 0.7 ),
+  purple: vec4(0.5, 0, 0.5, 0.7 ),
+  orange: vec4(1.0, 0.5, 0.0, 0.8),
+};
+
+var vertexColors = [
+  colorTmp.black,
+  //initial color
+  colorTmp.purple,
+  //--------
+  colorTmp.blue,
+  colorTmp.red,
+  colorTmp.orange,
+  colorTmp.green,
+  colorTmp.cyan,
+  colorTmp.white,
+
 ];
-var theta = [0, 0, 0];
-var modelViewMatrix = mat4();
-var modelViewMatrixLoc;
-var projectionMatrix = ortho(-10,10,-10,10,-10,10);
-var vBuffer;
-var cBuffer;
-var bBuffer;
-var vPosition;
-/*-----------PROPERTIES------------------------------*/   
+
+//Arm parameters
 var baseP = {
-    width: 5.0,
-    height: 2.0,
-    id:0,
+  width: 3.0,
+  height: 1.0,
+  id:0,
 };
 var lowerArmP = {
-    width: 0.5,
-    height: 5.0,
-    id: 1,
+  width: 0.5,
+  height: 5.0,
+  id: 1,
 };
 var upperArmP = {
-    width: 0.5,
-    height: 5.0,
-    id:2,
+  width: 0.5,
+  height: 5.0,
+  id:2,
 }
 var slider = {
-    body: 0,
-    lowerArm: 0,
-    upperArm: 0,
+  body: 0,
+  lowerArm: 0,
+  upperArm: 0,
 }
-var figure = [];
-var stack = [];
-var points = [];
-var colors = [];
-/*---------------------------------------------------*/
+var ballP = {
+  if: false, //if ball exists (only onclick)
+  radius: 0.2,
+  center: [],
+  colors: [],
+  vertices: [vec4(1,1,0,1.0),
+
+  vec4(1,2,0,1.0),
+  vec4(1.2,1.98,0,1.0),
+  vec4(1.4,1.917,0,1.0),
+  vec4(1.6,1.8,0,1.0),
+  vec4(1.8,1.6,0,1.0),
+  vec4(2,1,0,1.0),
+
+  vec4(1.8,0.4,0,1.0),
+  vec4(1.6,0.2,0,1.0),
+  vec4(1.4,0.083,0,1.0),
+  vec4(1.2,0.02,0,1.0),
+  vec4(1,0,0,1.0),
+
+  vec4(0.8,0.02,0,1.0),
+  vec4(0.6,0.083,0,1.0),
+  vec4(0.4,0.2,0,1.0),
+  vec4(0.2,0.4,0,1.0),
+  vec4(0,1,0,1.0),
+
+  vec4(0.2,1.6,0,1.0),
+  vec4(0.4,1.8,0,1.0),
+  vec4(0.6,1.917,0,1.0),
+  vec4(0.8,1.98,0,1.0),
+  vec4(1,2,0,1.0),],
+}
+for (var k =0; k<ballP.vertices.length;k++)
+  ballP.colors.push(colorTmp.red);
+
+
+var modelViewMatrix=mat4();
+var ballmodelViewMatrix= mat4();
+var projectionMatrix;
+
+var vBuffer, cBuffer,vPosition; //Arm buffer
+var modelViewMatrixLoc;
+var projectionMatrix = ortho(-10, 10, -10, 10, -10, 10);
+
+var bBuffer, vPositionB, cBufferB, vColorB; //ball buffers
+var modelViewMatrixBall = mat4();
+
+var theta= [ 0, 0, 0];
+var thetaTmp = {
+  base:0.0,
+  lowerArm:0.0,
+  upperArm:0.0,
+}
+
+//----------------------------------------------------------------------------
+
 function quad(  a,  b,  c,  d ) {
-    colors.push(vertexColor[a]);
+    colors.push(vertexColors[a]);
     points.push(vertices[a]);
-    colors.push(vertexColor[a]);
+    colors.push(vertexColors[a]);
     points.push(vertices[b]);
-    colors.push(vertexColor[a]);
+    colors.push(vertexColors[a]);
     points.push(vertices[c]);
-    colors.push(vertexColor[a]);
+    colors.push(vertexColors[a]);
     points.push(vertices[a]);
-    colors.push(vertexColor[a]);
+    colors.push(vertexColors[a]);
     points.push(vertices[c]);
-    colors.push(vertexColor[a]);
+    colors.push(vertexColors[a]);
     points.push(vertices[d]);
 }
+
 function colorCube() {
     quad( 1, 0, 3, 2 );
     quad( 2, 3, 7, 6 );
@@ -84,59 +149,47 @@ function colorCube() {
 
 /*------------RECURSIVE RENDERING--------------------*/
 /*From lecture notes #21 */
-function createNode(transform, render, sibling, child){
-    var node = {
-        transform: transform,
-        render: render,
-        sibling: sibling,
-        child: child
-    }
-    return node;
+var figure= [];
+var stack= [];
+
+function createNode(transform, render, sibling, child) {
+        var node={
+            transform: transform,
+            render: render,
+            sibling: sibling,
+            child: child
+        }
+
+        return node;
 }
-for(var i=0; i<3;i++)
-    figure[i] = createNode(null,null,null,null);
+
+for(var i=0; i<3;i++){
+    figure[i]= createNode(null,null,null,null);
+}
 
 function initNodes(id){
-    var m = mat4();
-    switch(id){
-        case baseP.id:
-            m = rotate(theta[baseP.id], 0, 1, 0);
-            figure[baseP.id]=createNode(m, base, null, lowerArmP.id);
-            break;
-        case lowerArmP.id:
-            m = translate(0.0, baseP.height, 0.0);
-            m = mult(m, rotate(theta[lowerArmP.id],0,0,1));
-            figure[lowerArmP.id]=createNode(m, lowerArm, null, upperArmP.id);
-            break;
-        case upperArm.id:
-            m = translate(0.0, lowerArmP.height, 0.0);
-            m = mult(m, rotate(theta[upperArmP.id],0,0,1));
-            figure[upperArmP.id]=createNode(m, upperArm, null, null);
-            break;
-    }
-}
-/*function initNodes(id){
     var m=mat4();
     switch(id){
-        case baseId:
-            m=rotate(theta[0],0,1,0);
-            figure[baseId]=createNode(m,base,null,lowerArmId)
+        case baseP.id:
+            m=rotate(theta[baseP.id],0,1,0);
+            figure[baseP.id]=createNode(m,base,null,lowerArmP.id)
             break;
 
-        case lowerArmId:
+        case lowerArmP.id:
             m = translate(0.0, baseP.height, 0.0);
-            m = mult(m, rotate(theta[1], 0, 0, 1 ));
-            figure[lowerArmId]=createNode(m,lowerArm,null,upperArmId)
+            m = mult(m, rotate(theta[lowerArmP.id], 0, 0, 1 ));
+            figure[lowerArmP.id]=createNode(m,lowerArm,null,upperArmP.id)
             break;
 
-        case upperArmId:
+        case upperArmP.id:
             m=translate(0.0, lowerArmP.height, 0.0);
-            m=mult(m, rotate(theta[2], 0, 0, 1) );
-            figure[upperArmId]=createNode(m,upperArm,null,null);
+            m=mult(m, rotate(theta[upperArmP.id], 0, 0, 1) );
+            figure[upperArmP.id]=createNode(m,upperArm,null,null);
             break;
 
     }
-}*/
+}
+
 function traverse(id){
     if(id == null)
         return;
@@ -155,32 +208,40 @@ function traverse(id){
 }
 /*---------------------------------------------------*/
 
+
+
+/*---------------------------------------------------*/
+
 window.onload = function init() {
 
     canvas = document.getElementById( "gl-canvas" );
-    
+
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.5, 0.5, 0.5, 1.0 );
-    gl.enable(gl.DEPTH_TEST);
+
+    gl.clearColor( 0.9, 0.9, 0.1, 0.4 );
+    gl.enable( gl.DEPTH_TEST );
 
     //
     //  Load shaders and initialize attribute buffers
     //
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
+
     gl.useProgram( program );
+
     colorCube();
-    
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );    
-    
-    // Associate out shader variables with our data buffer
+
+    // Create and initialize  buffer objects
+
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
     var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );   
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
 
     cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
@@ -188,31 +249,38 @@ window.onload = function init() {
 
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vColor ); 
+    gl.enableVertexAttribArray( vColor );
 
-    document.getElementById("sliderBody").onchange = function(event) {
-        // theta[baseId] = event.target.value;
-        slider.body=event.target.value;
-                // initNodes(baseId);
-    };
-    document.getElementById("sliderLower").onchange = function(event) {
-         // theta[lowerArmId] = event.target.value;
-                 // initNodes(lowerArmId);
-         slider.lowerArm=event.target.value;
-    };
-    document.getElementById("sliderUpper").onchange = function(event) {
-         // theta[upperArmId] =  event.target.value;
-                 // initNodes(upperArmId);
-         slider.upperArm=event.target.value;
-    };
+    bBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(ballP.vertices), gl.STATIC_DRAW );
 
-    for(var i=0; i<3;i++)
-        initNodes(i);
+    vPositionB = gl.getAttribLocation( program, "vPosition");
+    gl.vertexAttribPointer( vPositionB, 2, gl.FLOAT, false, 0, 0);
+
+    cBufferB = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBufferB );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(ballP.colors), gl.STATIC_DRAW );
+
+    document.getElementById("sliderBody").onchange = function() {
+        thetaTmp.base = event.target.value;
+    };
+    document.getElementById("sliderLower").onchange = function() {
+        thetaTmp.lowerArm = event.target.value;
+    };
+    document.getElementById("sliderUpper").onchange = function() {
+         thetaTmp.upperArm = event.target.value;
+    };
+    canvas.addEventListener("click", ball,false);
+
+        for(var i=0; i<3;i++){
+            initNodes(i);
+        }
     modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
-
     render();
-};
+}
 /*---------------------------------------------------*/
+
 function base() {
     var s = scalem(baseP.width, baseP.height, baseP.width);
     var instanceMatrix = mult( translate( 0.0, 0.5 * baseP.height, 0.0 ), s);
@@ -235,15 +303,198 @@ function upperArm() {
     gl.uniformMatrix4fv( modelViewMatrixLoc,  false, flatten(t) );
     gl.drawArrays( gl.TRIANGLES, 0, vertexCount );
 }
-/*---------------------------------------------------*/
 
-function render() {
+
+/*---------------------------------------------------*/
+var render = function() {
+
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.vertexAttribPointer(vPosition,4,gl.FLOAT,false,0,0);
     gl.uniformMatrix4fv( gl.getUniformLocation(program, "projectionMatrix"),  false, flatten(projectionMatrix) );
     gl.uniformMatrix4fv( modelViewMatrixLoc,  false, flatten(modelViewMatrix) );
-    traverse(baseP.id); 
 
+    traverse(baseP.id);
+
+    if(ballP.if){//if ball exists
+      drawBall();
+    }
+    else{
+      updateAnimation();
+
+    }
     requestAnimFrame(render);
 }
+function updateAnimation(){
+    if(theta[baseP.id]>thetaTmp.base)
+      theta[baseP.id]-=displancement;
+    else if(theta[baseP.id]<thetaTmp.base)
+      theta[baseP.id]+=displancement;
+    initNodes(baseP.id);
+
+    if(theta[lowerArmP.id]>thetaTmp.lowerArm)
+      theta[lowerArmP.id]-=displancement;
+    else if(theta[lowerArmP.id]<thetaTmp.lowerArm)
+      theta[lowerArmP.id]+=displancement;
+    initNodes(lowerArmP.id);
+
+    if(theta[upperArmP.id]>thetaTmp.upperArm)
+      theta[upperArmP.id]-=displancement;
+    else if(theta[upperArmP.id]<thetaTmp.upperArm)
+      theta[upperArmP.id]+=displancement; 
+    initNodes(upperArmP.id);
+}
+function drawBall(){
+      console.log("BALL EXISIT");
+      gl.bindBuffer(gl.ARRAY_BUFFER, bBuffer);
+      gl.vertexAttribPointer(vPositionB,4,gl.FLOAT,false,0,0);
+      gl.uniformMatrix4fv( gl.getUniformLocation(program, "projectionMatrix"),  false, flatten(projectionMatrix) );
+      gl.uniformMatrix4fv( gl.getUniformLocation(program, "modelViewMatrix"),  false, flatten(modelViewMatrixBall) );
+      gl.drawArrays(gl.TRIANGLE_FAN, 0, ballP.vertices.length);
+
+      //animating the arms to initia position
+      if (thetaL<0 && thetaU<0){
+        if(theta[lowerArmP.id]>=thetaL){
+          theta[lowerArmP.id]-=1;
+          initNodes(lowerArmP.id);
+        }
+        if(theta[upperArmP.id]>=thetaU){
+          theta[upperArmP.id]-=1;
+          initNodes(upperArmP.id);
+        }
+        if(theta[upperArmP.id]<=thetaU && (theta[lowerArmP.id]<=thetaL)){
+          ballP.if=false;
+          thetaTmp.base=0;
+          thetaTmp.lowerArm=0;
+          thetaTmp.upperArm=0;
+
+        }
+      }
+      else if (thetaL>0 && thetaU<0){
+        if(theta[lowerArmP.id]<=thetaL){
+          theta[lowerArmP.id]+=1;
+          initNodes(lowerArmP.id);
+        }
+        if(theta[upperArmP.id]>=thetaU){
+          theta[upperArmP.id]-=1;
+          initNodes(upperArmP.id);
+        }
+        if(theta[upperArmP.id]<=thetaU && (theta[lowerArmP.id]>=thetaL)){
+          ballP.if=false;
+          thetaTmp.base=0;
+          thetaTmp.lowerArm=0;
+          thetaTmp.upperArm=0;
+        }
+      }
+
+      else if (thetaL<0 && thetaU>0){
+        if(theta[lowerArmP.id]>=thetaL){
+          theta[lowerArmP.id]-=1;
+          initNodes(lowerArmP.id);
+        }
+        if(theta[upperArmP.id]<=thetaU){
+          theta[upperArmP.id]+=1;
+          initNodes(upperArmP.id);
+        }
+        if(theta[upperArmP.id]>=thetaU && (theta[lowerArmP.id]<=thetaL)){
+          ballP.if=false;
+          thetaTmp.base=0;
+          thetaTmp.lowerArm=0;
+          thetaTmp.upperArm=0;
+        }
+      }
+
+      else if (thetaL>0 && thetaU>0){
+        if(theta[lowerArmP.id]<=thetaL){
+          theta[lowerArmP.id]+=1;
+          initNodes(lowerArmP.id);
+
+
+        }
+        if(theta[upperArmP.id]<=thetaU){
+          theta[upperArmP.id]+=1;
+          initNodes(upperArmP.id);
+        }
+        if(theta[upperArmP.id]>=thetaU && (theta[lowerArmP.id]>=thetaL)){
+          ballP.if=false;
+          thetaTmp.base=0;
+          thetaTmp.lowerArm=0;
+          thetaTmp.upperArm=0;
+
+        }
+      }
+
+    }
+
+
+
+
+/*------------------BALL-----------------------------*/
+//get ball location and update theta[]
+function ball(){
+  //console.log(event.clientX+","+event.clientY);
+  ballP.if = true;
+  var rect = canvas.getBoundingClientRect();
+  ballP.center = [event.clientX-rect.left, event.clientY-rect.top];
+  //console.log(ballP.center);
+  modelViewMatrixBall = mult(scalem(0.35,0.35,1),mat4())
+
+  ballMouseLocation[0]=ballP.center[0];
+  ballMouseLocation[1]=ballP.center[1];
+  if(ballMouseLocation[0]<=256 && ballMouseLocation[1]<=256){ //top left
+    ballMouseLocation[0]=(-10*(256-ballMouseLocation[0])/256)-0.4
+    ballMouseLocation[1]=(10*(256-ballMouseLocation[1])/256)-0.5
+  }
+  else if(ballMouseLocation[0]>256 && ballMouseLocation[1]<=256){ //top right
+    ballMouseLocation[0]=(10*(ballMouseLocation[0]-256)/256)-0.2
+    ballMouseLocation[1]=(10*(256-ballMouseLocation[1])/256)-0.5
+  }
+  else if(ballMouseLocation[0]<=256 && ballMouseLocation[1]>256){ //bottom left
+    ballMouseLocation[0]=(-10*(256-ballMouseLocation[0])/256)-0.4
+    ballMouseLocation[1]=(10*(256-ballMouseLocation[1])/256)-0.5
+  }
+  else if(ballMouseLocation[0]>256 && ballMouseLocation[1]>256){ //bottom right
+    ballMouseLocation[0]=(10*(ballMouseLocation[0]-256)/256)-0.2
+    ballMouseLocation[1]=(10*(256-ballMouseLocation[1])/256)-0.5
+
+  }
+
+  //IK calculations-----------------------
+  var xe= ballMouseLocation[0]+0.4;
+  var ye= ballMouseLocation[1]-1.45;
+  var lLower= lowerArmP.height;
+  var lUpper= upperArmP.height;
+  var thetar= Math.acos((xe/(Math.sqrt((Math.pow(xe,2))+(Math.pow(ye,2))))));
+  document.getElementById("sliderBody").value="0";
+  document.getElementById("sliderLower").value="0";
+  document.getElementById("sliderUpper").value="0";
+  theta[baseP.id]=0;
+  theta[lowerArmP.id]=0;
+  theta[upperArmP.id]=0;
+  initNodes(baseP.id);
+  initNodes(lowerArmP.id);
+  initNodes(upperArmP.id);
+
+  //console.log(xe,ye)
+
+  thetaL=((thetar-(Math.acos(((Math.pow(lLower,2))+(Math.pow(xe,2))+(Math.pow(ye,2))-(Math.pow(lUpper,2)))/(2*lLower*Math.sqrt((Math.pow(xe,2))+(Math.pow(ye,2)))))))*(180/Math.PI))-90;
+  thetaU=(Math.PI-(Math.acos(((Math.pow(lLower,2))+(Math.pow(lUpper,2))-(Math.pow(xe,2))-(Math.pow(ye,2)))/(2*lLower*lUpper))))*(180/Math.PI);
+
+  if(ye<0){
+    ye=-(ballMouseLocation[1]-1.45);
+    xe=-(ballMouseLocation[0]+0.4);
+    thetar= Math.acos((xe/(Math.sqrt((Math.pow(xe,2))+(Math.pow(ye,2))))));
+    thetaL=180+(((thetar-(Math.acos(((Math.pow(lLower,2))+(Math.pow(xe,2))+(Math.pow(ye,2))-(Math.pow(lUpper,2)))/(2*lLower*Math.sqrt((Math.pow(xe,2))+(Math.pow(ye,2)))))))*(180/Math.PI))-90);
+    thetaU=(Math.PI-(Math.acos(((Math.pow(lLower,2))+(Math.pow(lUpper,2))-(Math.pow(xe,2))-(Math.pow(ye,2)))/(2*lLower*lUpper))))*(180/Math.PI);
+
+  }
+
+  //console.log("theta",thetaL, thetaU);
+
+  ballLive=true;
+  ballmodelViewMatrix= mult(translate(ballMouseLocation[0],ballMouseLocation[1],1),ballmodelViewMatrix)
+}
+/*---------------------------------------------------*/
+
+
